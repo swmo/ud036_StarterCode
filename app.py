f338149c2f3db54d04efd60ba68e060ab6d04e5e
Moses <momo@Mo.local>
2017-10-01 13:09:28 +0200

    add two more static movies, remove generated index.html, add index.html to .gitignore

--- app.py
+++ app.py
@@ -170,9 +170,16 @@ def open_movies_page(movies):
 
 movies = []
 
-movie = Movie('Intouchables','desc','https://upload.wikimedia.org/wikipedia/en/9/93/The_Intouchables.jpg','https://www.youtube.com/watch?v=34WIbmXkewU')
+movie = Movie('Intouchables','https://upload.wikimedia.org/wikipedia/en/9/93/The_Intouchables.jpg','https://www.youtube.com/watch?v=34WIbmXkewU')
+movies.append(movie)
 
+movie = Movie('Demain tout commence','https://img5.cdn.cinoche.com/images/f8746917134c8ebf68d72dc89fbdd146.jpg','https://www.youtube.com/watch?v=OAt0mmfW8S4')
+movies.append(movie)
+
+movie = Movie('Into the wild','http://www.gstatic.com/tv/thumb/movieposters/168385/p168385_p_v8_aa.jpg','https://www.youtube.com/watch?v=2LAuzT_x8Ek')
 movies.append(movie)
 
 open_movies_page(movies)
+
+
 
