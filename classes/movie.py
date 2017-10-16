aedceb722615e6f49eddfd51296d3ab8d7e4f284
Moses <momo@Mo.fritz.box>
2017-10-16 20:32:18 +0200

    PEP8 requirements or movie.pl

--- classes/movie.py
+++ classes/movie.py
@@ -1,17 +1,18 @@
 class Movie:
-        """     Class represent a movie
+    """ Class represent a movie
 
-                Attributes:
-                    title: Title of the movie
-                    poster_image_url: Url to an Image of the movie
-                    trailer_youtube_url: youtube like to the trailer of the movie
-         """
-        def __init__(self,title,poster_image_url,trailer_youtube_url):
-                """ Inits Movie Instance with the minimal informations """
-                self.title                  = title                 #Title of the movie
-                self.poster_image_url       = poster_image_url      #Url to the image of the movie
-                self.trailer_youtube_url    = trailer_youtube_url   #Url to a youtube trailer video
+        Attributes:
+            title: Title of the movie
+            poster_image_url: Url to an Image of
+                            the movie
+            trailer_youtube_url: youtube like to the trailer of the movie
+    """
+    def __init__(self, title, poster_image_url, trailer_youtube_url):
+        """ Inits Movie Instance with the minimal informations """
+        self.title = title  # Title of the movie
 
-        
+        # Url to the image of the movie
+        self.poster_image_url = poster_image_url
 
-
+        # Url to a youtube trailer video
+        self.trailer_youtube_url = trailer_youtube_url
